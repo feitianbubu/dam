92f87cc22b2f35ac3187e84513e06d49e14822f5
feitianbubu <feitianbubu@qq.com>
2025-09-29 10:46:05 +0800

    feat: enhance config file path resolution with fallback options

--- server/core/viper.go
+++ server/core/viper.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path"
 	"path/filepath"
 
 	"github.com/flipped-aurora/gin-vue-admin/server/core/internal"
@@ -70,6 +71,13 @@ func getConfigPath() (config string) {
 	if err != nil || os.IsNotExist(err) {
 		config = internal.ConfigDefaultFile
 		fmt.Printf("配置文件路径不存在, 使用默认配置文件路径: %s\n", config)
+		if _, err := os.Stat(config); err != nil || os.IsNotExist(err) {
+			config = path.Join("..", config)
+			fmt.Printf("默认配置文件路径不存在, 使用备用配置文件路径: %s\n", config)
+			if _, err := os.Stat(config); err != nil || os.IsNotExist(err) {
+				panic("未找到配置文件")
+			}
+		}
 	}
 
 	return
